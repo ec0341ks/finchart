a09b81502ae1f5a2e4d993b5c6a8bb64981f68e0
ec0341ks <50263756+ec0341ks@users.noreply.github.com>
2020-02-09 19:41:08 +0900

    admin画面の導入

--- techpit/finchart/admin.py
+++ techpit/finchart/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Company, Fstatement
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.Company):
+    list_display = ('name',)
+    list_display_links = ('name',)
+
+@admin.register(Fstatement)
+class FstatementAdmin(Fstatement):
+    list_display = ['company','fiscal_year',]
+    list_display_links = ['company',]
